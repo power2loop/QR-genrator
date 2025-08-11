8163c7f126142a88a20b9f1c04f1eb9c35231773
power2loop <kumar.vkvikash3@gmail.com>
2025-08-11 21:35:52 +0530

    qr-generator

---
+++ script.js
@@ -0,0 +1,15 @@
+let imgBox = document.querySelector(".img-box");
+let qrImage = document.getElementById("qr-image");
+let qrText = document.getElementById("qrText");
+
+function generateQRCode() {
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, timeout = 1000);
+    }
+}
